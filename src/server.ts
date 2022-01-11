b7d90de84bebf7fb44663028293da82f970fcb2a
Ilyas Ilyasov <ilyas.ilyasov.1@gmail.com>
2022-01-11 01:17:46 +0100

    endpoint for receiving messages, HTTP

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import * as express from "express";
-import * as path from "path";
 import * as moment from 'moment';
 import { Message } from "./models/Message";
 
@@ -7,10 +6,11 @@ const app = express();
 
 var cors = require('cors')
 
+var bodyParser = require('body-parser');
+
 app.use(cors())
-
+app.use(bodyParser({ limit: '50mb' }))
 app.use('/uploads', express.static('uploads'));
-
 app.set("port", process.env.PORT || 3000);
 
 let http = require("http").Server(app);
@@ -21,24 +21,19 @@ const io = require('socket.io')(http, {
     }
 });
 
-app.get("/", (req: any, res: any) => {
-    return 'Chat API';
-});
-
 const fs = require("fs");
 
 var userCount = 0;
 var lastMessages: any[] = [];
 var typingUsers: string[] = [];
 
-io.on("connection", (socket: any) => {
-
+io.on("connection", (socket: any) => 
+{
     socket.on('disconnect', () => {
         io.emit('user_count', --userCount);
     });
 
     socket.on("message",  (message: any) => {
-        
         let newMessage = new Message(message.sender, message.content, getCurrentTime(), (message.isImage || false), false);
         if(message.isImage)
         {
@@ -73,8 +68,26 @@ io.on("connection", (socket: any) => {
     io.emit('user_count', ++userCount);
 });
 
+app.post('/send-message', (req: any, res: any) => {
+    var message = req.body;
+    let newMessage = new Message(message.sender, message.content, getCurrentTime(), (message.isImage || false), false);
+    if (message.isImage) {
+        let uploadedFilename = storeImage(message.content);
+        newMessage.content = uploadedFilename;
+    }
+    io.emit('message', newMessage);
+    lastMessages.push(newMessage);
+
+    res.json({success: true});
+});
+
 app.get('/messages', (req: any, res: any) => {
     res.json({ messages: lastMessages });
+});
+
+app.get('/remove-messages', (req: any, res: any) => {
+    lastMessages.splice(0, lastMessages.length);
+    res.end('Messages deleted');
 });
 
 const getCurrentTime = () =>
